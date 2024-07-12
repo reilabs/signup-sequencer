29acb9f0d4cd4f203e3cd9141be68af155f7d5d7
Piotr Heilman <piotr.heilman@gmail.com>
2024-07-12 12:25:11 +0200

    wip

--- schemas/database/014_add_constraints_for_identities.up.sql
+++ schemas/database/014_add_constraints_for_identities.up.sql
@@ -11,18 +11,21 @@ ALTER TABLE identities ADD COLUMN pre_root BYTEA;
 -- This constraint ensures that we have consistent database and changes to the tre are done in a valid sequence.
 CREATE OR REPLACE FUNCTION validate_pre_root() returns trigger as $$
     DECLARE
-        last_root BYTEA;
+        last_id identities.id%type;
+        last_root identities.root%type;
     BEGIN
-        last_root := (
-            SELECT root
-            FROM identities
-            ORDER BY id DESC
-            LIMIT 1
-        );
+        SELECT id, root
+        INTO last_id, last_root
+        FROM identities
+        ORDER BY id DESC
+        LIMIT 1;
 
-        -- When last_root is NULL that means there are no records in identities table. The first prev_root can
+        -- When last_id is NULL that means there are no records in identities table. The first prev_root can
         -- be a value not referencing previous root in database.
-        IF last_root IS NULL THEN RETURN NEW;
+        IF last_id IS NULL THEN RETURN NEW;
+        END IF;
+
+        IF NEW.pre_root IS NULL THEN RAISE EXCEPTION 'Sent pre_root (%) can be null only for first record in table.', NEW.pre_root;
         END IF;
 
         IF (last_root != NEW.pre_root) THEN RAISE EXCEPTION 'Sent pre_root (%) is different than last root (%) in database.', NEW.pre_root, last_root;
@@ -31,4 +34,23 @@ CREATE OR REPLACE FUNCTION validate_pre_root() returns trigger as $$
         RETURN NEW;
     END;
 $$ language plpgsql;
+
 CREATE TRIGGER validate_pre_root_trigger BEFORE INSERT ON identities FOR EACH ROW EXECUTE PROCEDURE validate_pre_root();
+
+-- Below function took around 10 minutes for 10 million records.
+DO
+$do$
+DECLARE
+    prev_root identities.pre_root%type := NULL;
+    identity identities%rowtype;
+BEGIN
+    FOR identity IN SELECT * FROM identities ORDER BY id ASC
+    LOOP
+        IF identity.pre_root IS NULL THEN UPDATE identities SET pre_root = prev_root WHERE id = identity.id;
+        END IF;
+        prev_root = identity.root;
+    END LOOP;
+END
+$do$;
+
+CREATE UNIQUE INDEX idx_unique_pre_root on identities(pre_root) WHERE pre_root IS NOT NULL;
